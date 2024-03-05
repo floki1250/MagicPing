a8dd78224e7c695f9ad864a6bc4ea71504e763aa
Adem <37814393+floki1250@users.noreply.github.com>
2024-03-05 15:20:26 +0100

    UI

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,49 @@
 <template>
-    <div>
-        <header>
-            <div class="flex justify-between">
-                <div>Dashboard</div>
-                <div>Logo</div>
-                <div>
-                    <h1>Color mode: {{ $colorMode.value }}</h1>
-                    <select v-model="$colorMode.preference">
-                        <option value="system">System</option>
-                        <option value="light">Light</option>
-                        <option value="dark">Dark</option>
-                        <option value="sepia">Sepia</option>
-                    </select>
-                </div>
-            </div>
-        </header>
-        <div class="flex justify-center bg-white h-full">
-            <div
-                class="w-1/2 m-4 bg-rose-400/80 flex justify-center items-center flex-col rounded-3xl h-24 backdrop-blur-lg">
-                <div>
-                    <NuxtLink to="SendFile"> Sender</NuxtLink>
-                </div>
-                <div>
-                    <UButton color="white" variant="solid" block>Send</UButton>
-                </div>
-            </div>
-            <div class="w-1/2 m-4 bg-teal-400 flex justify-center items-center flex-col rounded-3xl h-3/4">
-                <NuxtLink to="ReceiveFile">Receiver</NuxtLink>
-                <UButton>Receive</UButton>
-            </div>
-
-            <h1>{{ data }}</h1>
+  <div>
+    <header>
+      <div class="flex justify-between">
+        <div>Dashboard</div>
+        <div>Logo</div>
+        <div>
+          <h1>Color mode: {{ $colorMode.value }}</h1>
+          <select v-model="$colorMode.preference">
+            <option value="system">System</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+            <option value="sepia">Sepia</option>
+          </select>
         </div>
+      </div>
+    </header>
+    <div class="flex justify-center bg-white h-full">
+      <div
+        class="w-1/2 m-4 bg-rose-400/80 flex justify-center items-center flex-col rounded-3xl h-24 frostedGlass border-white/15 border-2 shadow-xl"
+      >
+        <div>
+          <NuxtLink to="SendFile"> Sender</NuxtLink>
+        </div>
+        <div>
+          <UButton color="white" variant="solid" block>Send</UButton>
+        </div>
+      </div>
+      <div
+        class="w-1/2 m-4 bg-teal-400 flex justify-center items-center flex-col rounded-3xl h-3/4"
+      >
+        <NuxtLink to="ReceiveFile">Receiver</NuxtLink>
+        <UButton>Receive</UButton>
+      </div>
+
+      <h1>{{ data }}</h1>
     </div>
+  </div>
 </template>
 
 <script setup>
 const { data } = await useFetch("http://localhost:9000/peerjs/peers");
 </script>
+
+<style scope>
+.frostedGlass {
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+}
+</style>
