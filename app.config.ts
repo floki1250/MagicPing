224f0d4daf6c774a952459c59675573699b8ee23
floki1250 <d.adem1250@gmail.com>
2024-03-06 23:14:41 +0100

    UI

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   ui: {
-    primary: "blue",
+    primary: "amber",
     gray: "neutral",
   },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,42 @@
 <template>
   <div>
-    <header class="bg-[#61BFAD] text-white h-36">
+    <header class="bg-[#3d405b] text-white h-36">
       <div class="flex justify-between p-4">
         <div class="text-6xl font-bold font-sans">
           <h1>MagicDrop</h1>
         </div>
         <div class="text-6xl font-bold font-sans">
-          <UButton
-            :icon="$colorMode.value === 'dark' ? 'i-heroicons-moon' : 'i-heroicons-sun'"
-            size="xl"
-            color="white"
-            variant="link"
-            @click="switchColorMode"
-          />
+          <UButton :icon="$colorMode.value === 'dark' ? 'i-heroicons-moon' : 'i-heroicons-sun'" size="xl" color="white"
+            variant="link" @click="switchColorMode" />
         </div>
       </div>
     </header>
     <main>
-      <div class="w-full h-12 bg-red-500">Test</div>
+      <div class="w-full min-h-12 bg-[#f2cc8f] text-black p-4 flex justify-between">
+        <div>
+          <h1 class="font-bold text-2xl">RECEIVING</h1>
+          <p class="text-sm">Your device will shown as <b>X</b></p>
+        </div>
+        <div>
+          <UToggle size="md" :model-value="true" />
+        </div>
+      </div>
+
       <UAccordion :items="items" color="white" variant="solid" size="xl">
         <template #default="{ item, index, open }">
-          <UButton
-            :color="item.color"
-            variant="solid"
-            :ui="{ rounded: 'rounded-none', padding: { sm: 'p-3' } }"
-          >
+          <UButton color="white" variant="solid" :ui="{ rounded: 'rounded-none', padding: { sm: 'p-3' } }"
+            :class="`${item.color} hover:${item.color} border-none shadow-none`">
             <template #leading>
-              <div
-                class="w-6 h-6 rounded-full bg-primary-500 dark:bg-primary-400 flex items-center justify-center -my-1"
-              >
-                <UIcon :name="item.icon" class="w-4 h-4 text-white dark:text-gray-900" />
+              <div class="w-12 h-12 rounded-full flex items-center justify-center text-3xl">
+                <UIcon :name="item.icon" class="w-8 h-8 dark:text-white text-black" />
               </div>
             </template>
 
-            <span class="truncate">{{ index + 1 }}. {{ item.label }}</span>
+            <span class="truncate"> {{ item.label }}</span>
 
             <template #trailing>
-              <UIcon
-                name="i-heroicons-chevron-right-20-solid"
-                class="w-5 h-5 ms-auto transform transition-transform duration-200"
-                :class="[open && 'rotate-90']"
-              />
+              <UIcon name="i-heroicons-chevron-right-20-solid"
+                class="w-5 h-5 ms-auto transform transition-transform duration-200" :class="[open && 'rotate-90']" />
             </template>
           </UButton>
         </template>
@@ -54,46 +50,27 @@ const { data, error, refresh } = await useFetch("http://localhost:9000/peerjs/pe
 const colorMode = useColorMode();
 
 console.log(colorMode.preference);
-function switchColorMode() {
+function switchColorMode () {
   colorMode.value = colorMode.value === "dark" ? "light" : "dark";
 }
 const items = [
   {
+    label: "Theming",
+    icon: "i-heroicons-eye-dropper",
+    slot: "theming",
+    color: "bg-bay-leaf-300",
+  },
+  {
     label: "Getting Started",
     icon: "i-heroicons-information-circle",
     slot: "getting-started",
-    color: "red_(pantone)",
+    color: "bg-japonica-500",
   },
   {
     label: "Installation",
     icon: "i-heroicons-arrow-down-tray",
     slot: "installation",
-    color: "honeydew",
-  },
-  {
-    label: "Theming",
-    icon: "i-heroicons-eye-dropper",
-    defaultOpen: true,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.",
-  },
-  {
-    label: "Layouts",
-    icon: "i-heroicons-rectangle-group",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.",
-  },
-  {
-    label: "Components",
-    icon: "i-heroicons-square-3-stack-3d",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.",
-  },
-  {
-    label: "Utilities",
-    icon: "i-heroicons-wrench-screwdriver",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.",
+    color: "bg-quarter-spanish-white-50",
   },
 ];
 </script>
