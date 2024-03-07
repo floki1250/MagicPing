d335b7c9663bb77ca3fb80ef6d464c4a0c84c9fb
Adem <37814393+floki1250@users.noreply.github.com>
2024-03-07 18:03:13 +0100

    UI

--- pages/index.vue
+++ pages/index.vue
@@ -21,38 +21,22 @@
           <UToggle size="md" :model-value="true" />
         </div>
       </div>
-
-      <UAccordion :items="items" color="white" variant="solid" size="xl">
-        <template #default="{ item, index, open }">
-          <UButton color="white" variant="solid" :ui="{ rounded: 'rounded-none', padding: { sm: 'p-3' } }"
-            :class="`${item.color} hover:${item.color} border-none shadow-none`">
-            <template #leading>
-              <div class="w-12 h-12 rounded-full flex items-center justify-center text-3xl">
-                <UIcon :name="item.icon" class="w-8 h-8 dark:text-white text-black" />
-              </div>
-            </template>
-
-            <span class="truncate"> {{ item.label }}</span>
-
-            <template #trailing>
-              <UIcon name="i-heroicons-chevron-right-20-solid"
-                class="w-5 h-5 ms-auto transform transition-transform duration-200" :class="[open && 'rotate-90']" />
-            </template>
-          </UButton>
-        </template>
-      </UAccordion>
+      <div class="divide-y divide-slate-200">
+        <Accordion />
+      </div>
     </main>
   </div>
 </template>
 
 <script setup>
-const { data, error, refresh } = await useFetch("http://localhost:9000/peerjs/peers");
+
+
 const colorMode = useColorMode();
 
-console.log(colorMode.preference);
 function switchColorMode () {
   colorMode.value = colorMode.value === "dark" ? "light" : "dark";
 }
+
 const items = [
   {
     label: "Theming",
