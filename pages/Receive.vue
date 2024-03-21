08c244b700f6753b7c53c1cf6efe5562e0a90e4e
Adem <37814393+floki1250@users.noreply.github.com>
2024-03-21 14:47:59 +0100

    Fix File TRansfer

--- pages/Receive.vue
+++ pages/Receive.vue
@@ -1,16 +1,18 @@
 <template>
-    <div>
-        <h2>
-            Receiver
-            <p>{{ characterName }}</p>
-            <img :src="qrcode" alt="QR Code" v-if="qrcode" />
-            <UButton @click="handleFileDownload" v-if="receivedFile">Download : {{ receivedFile.name }}</UButton>
-            <p class="text-gray-500 flex flex-wrap w-40">{{ receivedFile }}</p>
-            <hr />
-            <p v-if="messageReceived">{{ messageReceived }}</p>
-        </h2>
-        <hr />
-    </div>
+  <div>
+    <h2>
+      Receiver
+      <p>{{ characterName }}</p>
+      <img :src="qrcode" alt="QR Code" v-if="qrcode" />
+      <UButton @click="handleFileDownload" v-if="receivedFile"
+        >Download : {{ receivedFile.name }}</UButton
+      >
+      <p class="text-gray-500 flex flex-wrap w-40">{{ receivedFile }}</p>
+      <hr />
+      <p v-if="messageReceived">{{ messageReceived }}</p>
+    </h2>
+    <hr />
+  </div>
 </template>
 
 <script setup>
@@ -21,66 +23,42 @@ import Peer from "peerjs";
 import { uniqueNamesGenerator, starWars, adjectives } from "unique-names-generator";
 
 const characterName = uniqueNamesGenerator({
-    dictionaries: [adjectives, starWars],
+  dictionaries: [adjectives, starWars],
 }).replace(/\s/g, "");
 let qrcode = useQRCode(characterName);
 const messageReceived = ref("");
 const myPeer = new Peer(characterName, {});
 const receivedFile = ref(null);
-const receivedChunks = ref([]); const receivedFile = ref(null);
 
 const chunkSize = 16 * 1024;
 onMounted(() => {
-    myPeer.on("connection", (conn) => {
-        conn.on("data", (data) => {
-            if (typeof data === "string") {
-                messageReceived.value = data;
-                console.log(typeof data);
-                if (typeof data === "object") {
-                    // Check for metadata
-                    receivedFile.value = data;
-                } else {
-                    receivedChunks.value.push(data);
-                }
+  myPeer.on("connection", (conn) => {
+    conn.on("data", (data) => {
+      if (typeof data === "string") {
+        messageReceived.value = data;
+      } else {
+        receivedFile.value = data;
+      }
 
-                if (
-                    receivedChunks.value.length === Math.ceil(receivedFile.value.size / (1024 * 1024))
-                ) {
-                    // Download completed, trigger download logic
-                    const blob = new Blob(receivedChunks.value, {
-                        type: receivedFile.value.type,
-                    });
-                    const url = window.URL.createObjectURL(blob);
-                    const link = document.createElement("a");
-                    link.href = url;
-                    link.download = receivedFile.value.name;
-                    link.click();
-
-                    window.URL.revokeObjectURL(url);
-                }
-            } else {
-                receivedFile.value = data;
-            }
-
-            if (
-                receivedFile.value.chunks.length ===
-                Math.ceil(receivedFile.value.size / chunkSize)
-            ) {
-                handleFileDownload();
-            }
-        });
+      if (
+        receivedFile.value.chunks.length ===
+        Math.ceil(receivedFile.value.size / chunkSize)
+      ) {
+        handleFileDownload();
+      }
     });
+  });
 });
-async function handleFileDownload () {
-    const blob = new Blob(receivedFile.value.chunks, {
-        type: receivedFile.value.type,
-    });
+async function handleFileDownload() {
+  const blob = new Blob(receivedFile.value.chunks, {
+    type: receivedFile.value.type,
+  });
 
-    const url = window.URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = receivedFile.value.name;
-    link.click();
-    window.URL.revokeObjectURL(url); // Clean up memory leak
+  const url = window.URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = receivedFile.value.name;
+  link.click();
+  window.URL.revokeObjectURL(url); // Clean up memory leak
 }
 </script>
