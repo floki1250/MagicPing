978f13b4f9a27f43cd1a43b33c5cd4a7339ad867
floki1250 <d.adem1250@gmail.com>
2024-03-25 00:41:49 +0100

    blur-bub

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,15 @@
 <template>
-    <UContainer as="div">
+
+    <UContainer as="div" class="h-screen  overflow-hidden">
+        <div class="relative ">
+
+            <div class="blur-blob absolute top-16 left-16 bg-teal-200 dark:bg-teal-500  w-96 h-96 ">
+            </div>
+            <div class="blur-blob absolute top-16 right-16 bg-amber-200 dark:bg-amber-500   w-96 h-96"></div>
+        </div>
+
         <header
-            class="dark:bg-black/70  bg-white/50 border border-gray-50 dark:border-gray-500  h-full m-4 rounded-lg backdrop-blur-lg awesome-shadow dark:shadow-lg">
+            class="dark:bg-black/70  bg-white/50 border border-gray-100 dark:border-gray-900 hover:dark:border-gray-600 hover:border-gray-300 transition ease-in-out duration-500 m-4 rounded-lg backdrop-blur-lg awesome-shadow dark:shadow-lg h-fit">
             <div class="flex justify-between p-4">
                 <div class="text-3xl font-bold">
                     <NuxtLink to="/">
@@ -17,7 +25,9 @@
         </header>
 
         <slot />
-
+        <footer class="flex justify-center content-end bg-black">
+            footer
+        </footer>
 
     </UContainer>
 </template>
