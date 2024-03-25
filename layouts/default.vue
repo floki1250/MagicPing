3f66635faa949fb8713ee005e4e923c65e7c82b8
Adem <37814393+floki1250@users.noreply.github.com>
2024-03-25 14:53:30 +0100

    ui

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
 <template>
 
-    <UContainer as="div" class="h-screen  overflow-hidden">
-        <div class="relative ">
-
-            <div class="blur-blob absolute top-16 left-16 bg-teal-200 dark:bg-teal-500  w-96 h-96 ">
+    <UContainer as="div" class="min-h-screen  overflow-hidden">
+        <div class="relative hidden lg:block">
+            <div
+                class="blur-blob absolute top-10 left-5 md:top-16 md:left-16 lg:top-16 lg:left-16 bg-teal-200 dark:bg-teal-500 w-60 h-60 md:w-40 md:h-40 sm:w-40 sm:h-40 lg:w-96 lg:h-96 ">
             </div>
-            <div class="blur-blob absolute top-16 right-16 bg-amber-200 dark:bg-amber-500   w-96 h-96"></div>
+            <div
+                class="blur-blob absolute top-10 right-5 md:top-16 md:right-16 lg:top-16 lg:right-16 bg-rose-200 dark:bg-rose-800 w-60 h-60 md:w-40 md:h-40 sm:w-40 sm:h-40  lg:w-96 lg:h-96">
+            </div>
         </div>
 
         <header
@@ -25,8 +27,14 @@
         </header>
 
         <slot />
-        <footer class="flex justify-center content-end bg-black">
-            footer
+        <footer class="flex justify-center  absolute bottom-0 ">
+
+            <div
+                class="dark:bg-black/70  bg-white/50 border border-gray-100 dark:border-gray-900 hover:dark:border-gray-600 hover:border-gray-300 transition ease-in-out duration-500 m-4 rounded-lg backdrop-blur-lg awesome-shadow dark:shadow-lg h-fit">
+                <NuxtLink to="/">
+                    <h1 class="righteous dark:text-white text-black/70 ml-2 mt-1">MagicPing</h1>
+                </NuxtLink>
+            </div>
         </footer>
 
     </UContainer>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,8 @@
 <template>
   <main class="p-4 transition duration-300">
-
-    <div class="flex w-full mb-2">
+    <div class="flex w-full mb-2 flex-col lg:flex-row justify-center content-center ">
       <div @mouseenter="hoverSend = true" @mouseleave="hoverSend = false"
-        class="w-1/2 h-60 mr-1 hover:w-11/12 text-center flex justify-center items-center dark:bg-black/70 dark:text-white bg-white/50 text-black/70 rounded-lg backdrop-blur-lg transition-all ease-in-out duration-500 border border-gray-100 dark:border-gray-900 hover:dark:border-gray-600 hover:border-gray-300   awesome-shadow dark:shadow-lg">
+        class="mb-2 w-full lg:w-1/2 h-60 lg:mr-1 hover:w-11/12 text-center flex justify-center items-center dark:bg-black/70 dark:text-white bg-white/50 text-black/70 rounded-lg backdrop-blur-lg transition-all ease-in-out duration-500 border border-gray-100 dark:border-gray-900 hover:dark:border-gray-600 hover:border-gray-300   awesome-shadow dark:shadow-lg">
         <NuxtLink to="/Send" class="w-full h-full flex justify-center items-center">
           <div class="flex" :class="hoverReceive ? 'rotate-90 w-fit ' : 'rotate-0'">
             <UIcon name="i-line-md-uploading-loop" class="w-10 h-10 mr-2" />
@@ -14,7 +13,7 @@
         </NuxtLink>
       </div>
       <div @mouseenter="hoverReceive = true" @mouseleave="hoverReceive = false"
-        class="w-1/2 h-60 rounded-lg ml-1 hover:w-11/12 dark:bg-black/70 dark:text-white bg-white/50 text-black/70 backdrop-blur-lg transition-all ease-in-out duration-500 border border-gray-100 dark:border-gray-900 hover:dark:border-gray-600 hover:border-gray-300  awesome-shadow dark:shadow-lg">
+        class=" w-full lg:w-1/2  h-60 rounded-lg lg:ml-1 hover:w-11/12 dark:bg-black/70 dark:text-white bg-white/50 text-black/70 backdrop-blur-lg transition-all ease-in-out duration-500 border border-gray-100 dark:border-gray-900 hover:dark:border-gray-600 hover:border-gray-300  awesome-shadow dark:shadow-lg">
         <NuxtLink to="/Receive" class="w-full h-full flex justify-center items-center">
           <div class="flex" :class="hoverSend ? 'rotate-90 w-fit ' : 'rotate-0'">
             <UIcon name="i-line-md-downloading-loop" class="w-10 h-10 mr-2" />
