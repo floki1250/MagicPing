8281a289356b95d1d8f9b2aec2f9d6e830ef21d9
floki1250 <d.adem1250@gmail.com>
2024-03-26 09:03:52 +0100

    ui

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
 
     <UContainer as="div" class="min-h-screen  overflow-hidden">
-        <div class="relative hidden lg:block">
+        <div class="relative  lg:block">
             <div
-                class="blur-blob absolute top-10 left-5 md:top-16 md:left-16 lg:top-16 lg:left-16 bg-teal-200 dark:bg-teal-500 w-60 h-60 md:w-40 md:h-40 sm:w-40 sm:h-40 lg:w-96 lg:h-96 ">
+                class="blur-blob absolute top-20 left-20 md:top-20 md:left-16 lg:top-20 lg:left-16 bg-teal-200 dark:bg-teal-500 w-60 h-60 md:w-40 md:h-40 sm:w-40 sm:h-40 lg:w-96 lg:h-96 ">
             </div>
             <div
-                class="blur-blob absolute top-10 right-5 md:top-16 md:right-16 lg:top-16 lg:right-16 bg-rose-200 dark:bg-rose-800 w-60 h-60 md:w-40 md:h-40 sm:w-40 sm:h-40  lg:w-96 lg:h-96">
+                class="blur-blob absolute top-96 left-16 md:top-20 md:left-2/3 lg:top-20 lg:left-2/3 bg-rose-200 dark:bg-rose-800 w-60 h-60 md:w-40 md:h-40 sm:w-40 sm:h-40  lg:w-96 lg:h-96">
             </div>
         </div>
 
@@ -27,17 +27,23 @@
         </header>
 
         <slot />
-        <footer class="flex justify-center  absolute bottom-0 ">
 
-            <div
-                class="dark:bg-black/70  bg-white/50 border border-gray-100 dark:border-gray-900 hover:dark:border-gray-600 hover:border-gray-300 transition ease-in-out duration-500 m-4 rounded-lg backdrop-blur-lg awesome-shadow dark:shadow-lg h-fit">
-                <NuxtLink to="/">
-                    <h1 class="righteous dark:text-white text-black/70 ml-2 mt-1">MagicPing</h1>
-                </NuxtLink>
-            </div>
-        </footer>
 
     </UContainer>
+    <footer class="absolute bottom-0 ">
+
+        <div>
+            <NuxtLink to="https://github.com/floki1250" target="_blank">
+                <UIcon name="i-line-md-github-loop" class="w-6 h-6 m-2" />
+            </NuxtLink>
+            <NuxtLink to="https://twitter.com/Adem1250_Dr" target="_blank">
+                <UIcon name="i-line-md-twitter-x" class="w-6 h-6 m-2" />
+            </NuxtLink>
+            <NuxtLink to="https://adamdar.vercel.app/" target="_blank">
+                <UIcon name="i-line-md-star-alt-twotone" class="w-6 h-6 m-2" />
+            </NuxtLink>
+        </div>
+    </footer>
 </template>
 
 <script setup>
