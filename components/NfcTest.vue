f01c2d68bcdac03eba3f9406d338af9829afa193
Adem <37814393+floki1250@users.noreply.github.com>
2024-10-05 10:47:08 +0100

    Fixes

--- components/NfcTest.vue
+++ components/NfcTest.vue
@@ -1,29 +1,60 @@
 <template>
     <div>
-        <div v-if="nfcMessage">
-            NFC Message: {{ nfcMessage }}
-        </div>
-        <div v-if="errorMessage">
-            Error: {{ errorMessage }}
-        </div>
-        <div v-if="writeStatus">
-            {{ writeStatus }}
-        </div>
-
-        <UInput v-model="messageToWrite" placeholder="Enter message to write" />
-        <div class="grid grid-cols-3 gap-2">
-            <UButton @click="writeNFC(messageToWrite)">Write NFC</UButton>
-            <UButton @click="readNFC">Read NFC</UButton>
-            <UButton @click="stopNFC">Stop NFC</UButton>
-        </div>
-
+        <h1>NFC Example</h1>
+        <button @click="writeNFC">Write NFC Tag</button>
+        <p v-if="nfcMessage">NFC Message: {{ nfcMessage }}</p>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { useNFC } from '~/composables/useNFC'
+import { ref, onMounted, onBeforeUnmount } from 'vue';
 
-const { nfcMessage, errorMessage, writeStatus, readNFC, writeNFC, stopNFC } = useNFC()
-const messageToWrite = ref('')
+// Reactive state to hold NFC message
+const nfcMessage = ref('');
+
+// Function to handle NFC reading
+const onNFCRead = (nfcEvent) => {
+    const tag = nfcEvent.tag;
+    nfcMessage.value = tag.ndefMessage
+        ? nfc.bytesToString(tag.ndefMessage[0].payload)
+        : 'No NDEF data';
+};
+
+// Function to write to NFC tag
+const writeNFC = () => {
+    const message = [
+        ndef.textRecord('Hello from Nuxt 3 with Cordova!')
+    ];
+
+    nfc.write(message).then(() => {
+        alert('NFC Tag written successfully!');
+    }).catch(err => {
+        console.error('Failed to write NFC tag:', err);
+    });
+};
+
+onMounted(() => {
+    if (window.cordova) {
+        document.addEventListener('deviceready', () => {
+            console.log('Device is ready, initializing NFC...');
+            // Initialize NFC listener
+            nfc.addNdefListener(onNFCRead, () => {
+                console.log('Listening for NFC Tags...');
+            }, (err) => {
+                console.error('Error adding NFC listener:', err);
+            });
+        }, false);
+    }
+});
+
+onBeforeUnmount(() => {
+    if (window.cordova) {
+        document.removeEventListener('deviceready', null);
+        nfc.removeNdefListener(onNFCRead, () => {
+            console.log('Stopped NFC listener');
+        }, (err) => {
+            console.error('Error removing NFC listener:', err);
+        });
+    }
+});
 </script>
---
+++ pages/[...catchall].vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+if (window.location.pathname === '/index.html') {
+    router.push('/')
+}
+</script>
